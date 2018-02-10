ccc76f3264735292c141c6a516dd374410f5314f
Bryan0xFF <bjmejia@correo.url.edu.gt>
2018-02-09 22:01:25 -0600

    Agregar botones Jugadores/Index

---
+++ Lab01_1252016_1053016/Lab01_1252016_1053016/Controllers/JugadoresController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01_1252016_1053016.Controllers
+{
+    public class JugadoresController : Controller
+    {
+        // GET: Jugadores
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        internal ActionResult ListaGenerica()
+        {
+
+            return View();
+        }
+
+        internal ActionResult ListaNET()
+        {
+            return View();
+        }
+
+        // GET: Jugadores/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Jugadores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jugadores/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Jugadores/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Jugadores/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Jugadores/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Jugadores/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Lab01_1252016_1053016/Lab01_1252016_1053016/Models/Jugador.cs
+++ Lab01_1252016_1053016/Lab01_1252016_1053016/Models/Jugador.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,15 @@ namespace Lab01_1252016_1053016.Models
 {
     public class Jugador
     {
+        [Display(Name = "Nombre del Jugador:"), Required]
         public string Nombre { get; set; }
+        [Display(Name = "Apellido del Jugador:"), Required]
         public string Apellido { get; set; }
+        [Display(Name = "Posicion:"), Required]
         public string Posicion { get; set; }
+        [Display(Name = "Salario:"), Required]
         public double Salario { get; set; }
+        [Display(Name = "Club actual:"), Required]
         public string Club { get; set; }
 
         public Jugador(string nombre, string apellido, string posicion, double salario, string club)
